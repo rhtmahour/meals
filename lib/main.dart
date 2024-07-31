b686b84f932956c917f1f93bf12a45e9c8e62878
rhtmahour <rhtmahour@gmail.com>
2024-07-31 18:54:14 +0530

    Todays code on 31-07-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/screens/categories.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +24,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const MealsScreen(
+        title: 'Some Category...',
+        meals: dummyMeals,
+      ),
     );
   }
 }
