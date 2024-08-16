43f4fbd6e5c0dac7bd32c4b7ee74cfa1a14bf71b
rhtmahour <rhtmahour@gmail.com>
2024-08-16 17:02:52 +0530

    todays code on 16-08-2024

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/category_grid_item.dart';
@@ -80,11 +79,18 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-          padding: EdgeInsets.only(
-            top: 100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
           ),
-          child: child),
+        ),
+        child: child,
+      ),
     );
   }
 }
